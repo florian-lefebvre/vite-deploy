886e3934b7cbedb65fb3beb31e4c718c5aff04d8
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-04-24 09:40:51 +0200

    feat: more cloudflare examples

---
+++ examples/clouflare-tanstack-start/vite.config.ts
@@ -0,0 +1,28 @@
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import cloudflare from "@vite-deploy/cloudflare"
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [
+    tailwindcss(),
+    tanstackStart({
+      srcDirectory: "src",
+    }),
+    viteReact(),
+    cloudflare({
+      output: "hybrid",
+      prerender: {
+        entrypoint: "./src/prerender.ts",
+        format: "directory",
+      },
+    }),
+  ],
+});
