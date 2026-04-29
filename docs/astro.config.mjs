982016e12350e070c5a0e09b0fb0e7f502795273
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-04-29 16:07:49 +0200

    update site

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,8 +7,7 @@ import starlightLinksValidator from "starlight-links-validator";
 /* https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables */
 const NETLIFY_PREVIEW_SITE = process.env.CONTEXT !== 'production' && process.env.DEPLOY_PRIME_URL;
 
-// TODO: update
-const site = NETLIFY_PREVIEW_SITE || "https://iridescent-klepon-88ac7e.netlify.app/";
+const site = NETLIFY_PREVIEW_SITE || "https://vite-deploy.web-runes.dev/";
 
 // https://astro.build/config
 export default defineConfig({
